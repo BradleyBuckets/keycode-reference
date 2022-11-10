9d19b3ed6bb443ab06abd587781b175dd4990171
Bradley Griffin <bigriff11@gmail.com>
2022-11-10 08:21:13 -0700

    Add explanation html and style it

--- keyref.js
+++ keyref.js
@@ -1,5 +1,6 @@
 let text = document.querySelector('.text')
 let elementName = document.querySelector('.name')
+let title = document.querySelector('.title')
 
 document.addEventListener("keyup", e => {
     text.textContent = `"${e.key}"`;
